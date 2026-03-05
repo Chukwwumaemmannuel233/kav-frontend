62f2a506072a966dfcc6994487a4dea7007671bd
Chukwuma <echukwuma561@gmail.com>
2026-03-05 07:56:40 +0100

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="color-scheme" content="light" />
+        <meta name="color-scheme" content="light dark" />
       </head>
       <body className={`font-sans antialiased`}>
         <LoadingProvider>
